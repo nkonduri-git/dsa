007c8a8d8d8f0d138d8c1023edec25fdc0c13fd6
karthik <knvkarthik01@gmail.com>
2025-02-08 18:37:21 -0500

    third change

--- #basics.py
+++ #basics.py
@@ -35,7 +35,7 @@ person('navin',age=28,city='mumbai',phonenumber=908989)
 a=10
 def something():
    #global a
-   x=globals()['a']# when we want to acess global and local variable in same function
+   cx=globals()['a']# when we want to acess global and local variable in same function
    a=90
    b=9
 
@@ -44,3 +44,71 @@ def something():
 something()
 print(a)
 print("outside",a)
+#passing list to a function
+def count(lst):
+    even=0
+    odd=0
+
+    for i in lst:
+        if i%2==0:
+            even+=1
+        else:
+            odd+=1
+    return even,odd
+
+lst=[20,34,14,19,16,24,28,47,26]
+
+even,odd=count(lst)
+
+print("Even:() and odd:()".format(even,odd))
+#fibanoci 
+def fib(n):
+    a=0
+    b=1
+
+    if n==1:
+        print(a)
+
+    else:
+        print(a)
+        print(b)
+        
+
+    print(a)
+    print(b)
+
+    for i in range(2,n):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+
+
+fib(5)
+
+#factorial of a number
+
+
+def fact(n):
+    p=1
+    
+    for i in range(1,n+1):
+         p=n*i
+    
+    return p
+
+l=fact(5)
+print(l)
+#recursion of factorial
+
+def fact(n):
+
+    if n==0:
+        return 1
+    return n*fact(n-1)
+
+
+
+
+result=fact(5)
+print(result)
